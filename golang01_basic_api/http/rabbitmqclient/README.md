b462e1aaec29d57b68986456f3619da34e723c59
youxia999 <jingzhouyouxia@gmail.com>
2019-11-30 15:13:52 +0800

    更新read me

--- golang01_basic_api/http/rabbitmqclient/README.md
+++
@@ -1,8 +0,0 @@
-# 项目作用
-访问rabbitmq rest api，得到一些数据，后续可以根据这些数据作监控告警功能。
-
-# 最新的rabbitmq api文档地址
-https://rawcdn.githack.com/rabbitmq/rabbitmq-management/v3.8.1/priv/www/api/index.html
-
-# 参考资料
-https://supereagle.github.io/2017/11/22/request-with-auth/
--- golang01_basic_api/http/rabbitmqclient/client.go
+++ golang01_basic_api/http/rabbitmqclient/client.go
@@ -6,7 +6,11 @@ import (
 	"io"
 	"net/http"
 )
-
+/**
+1.代码作用: 访问rabbitmq rest api，得到一些数据。后续可以根据这些数据作监控告警功能。
+2.最新的rabbitmq api文档地址: https://rawcdn.githack.com/rabbitmq/rabbitmq-management/v3.8.1/priv/www/api/index.html
+3.参考资料: https://supereagle.github.io/2017/11/22/request-with-auth/
+ */
 func main() {
 	result,err:=do("GET","http://192.168.172.3:15672/api/connections",nil);
 	if err!=nil {
