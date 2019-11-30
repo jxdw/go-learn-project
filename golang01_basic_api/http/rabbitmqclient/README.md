5afa94e4664d5c5cff72eceac46e04171c83090a
youxia999 <jingzhouyouxia@gmail.com>
2019-11-30 15:07:01 +0800

    更新read me

---
+++ golang01_basic_api/http/rabbitmqclient/README.md
@@ -0,0 +1,5 @@
+# 项目作用
+访问rabbitmq rest api，得到一些数据，后续可以根据这些数据作监控告警功能。
+
+# 最新的rabbitmq api文档地址
+https://rawcdn.githack.com/rabbitmq/rabbitmq-management/v3.8.1/priv/www/api/index.html
---
+++ golang01_basic_api/http/rabbitmqclient/client.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	result,err:=do("GET","http://192.168.172.3:15672/api/vhosts",nil);
+	if err!=nil {
+		panic(err)
+	}
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(result.Body)
+	fmt.Println(buf.String())
+}
+func do(method string,url string,playload io.Reader) (*http.Response,error)  {
+	req,err:=http.NewRequest(method,url,playload)
+	if err!=nil {
+		return nil,err
+	}
+	req.SetBasicAuth("admin","admin")
+	return  http.DefaultClient.Do(req)
+}
