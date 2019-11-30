59b105dc8cde6cb87c40fbd2fc8ebc03ba0bd587
youxia999 <jingzhouyouxia@gmail.com>
2019-11-30 15:49:18 +0800

    update comment

--- golang01_basic_api/http/rabbitmqclient/client.go
+++ golang01_basic_api/http/rabbitmqclient/client.go
@@ -6,25 +6,26 @@ import (
 	"io"
 	"net/http"
 )
+
 /**
 1.代码作用: 访问rabbitmq rest api，得到一些数据。后续可以根据这些数据作监控告警功能。
 2.最新的rabbitmq api文档地址: https://rawcdn.githack.com/rabbitmq/rabbitmq-management/v3.8.1/priv/www/api/index.html
 3.参考资料: https://supereagle.github.io/2017/11/22/request-with-auth/
- */
+*/
 func main() {
-	result,err:=do("GET","http://192.168.172.3:15672/api/connections",nil);
-	if err!=nil {
+	result, err := do("GET", "http://192.168.172.3:15672/api/connections", nil)
+	if err != nil {
 		panic(err)
 	}
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(result.Body)
 	fmt.Println(buf.String())
 }
-func do(method string,url string,playload io.Reader) (*http.Response,error)  {
-	req,err:=http.NewRequest(method,url,playload)
-	if err!=nil {
-		return nil,err
+func do(method string, url string, playload io.Reader) (*http.Response, error) {
+	req, err := http.NewRequest(method, url, playload)
+	if err != nil {
+		return nil, err
 	}
-	req.SetBasicAuth("admin","admin")
-	return  http.DefaultClient.Do(req)
+	req.SetBasicAuth("admin", "admin")
+	return http.DefaultClient.Do(req)
 }
