82da17ee2b3e573d1bedfb32e1cc8d2c5c62c5c7
youxia999 <jingzhouyouxia@gmail.com>
2020-02-06 15:43:29 +0800

    commit

--- golang01_basic_api/http/filter.go
+++ golang01_basic_api/http/filter.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strconv"
 )
 //实现http.HandlerFunc接口，
 //函数签名符合http.HandlerFunc
 func hello(w http.ResponseWriter,request *http.Request){
+	s,_:=ioutil.ReadAll(request.Body)
+	body:=string(s)
+	fmt.Println("second")
+
+	fmt.Println(body)
 	w.Write([]byte("hello world"))
 }
 
 func log(hf http.HandlerFunc) http.HandlerFunc{
 	count:=0;
 	return func(writer http.ResponseWriter, request *http.Request) {
+		fmt.Println("first")
+		s,_:=ioutil.ReadAll(request.Body)
+		body:=string(s)
+		fmt.Println(body)
+		request.Body = ioutil.NopCloser(bytes.NewBuffer(s))
 		count++;
 		invokeinfo:="handler function  call "+strconv.Itoa(count)+" times"
 		fmt.Println(invokeinfo)
